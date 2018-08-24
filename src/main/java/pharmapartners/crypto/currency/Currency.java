f82dfbb5e40a15a930390c4cf8b7ea3242051a97
KdR88 <k.a.d.ridder@student.tue.nl>
2018-08-24 23:19:52 +0200

    Create Currency builder

--- src/main/java/pharmapartners/crypto/currency/Currency.java
+++ src/main/java/pharmapartners/crypto/currency/Currency.java
@@ -7,5 +7,43 @@ public class Currency {
     private Long numberOfCoins;
     private Long marketCap;
     
-    public Currency() {}
+	public Long getId() {
+		return id;
+	}
+    
+    public String getTicker() {
+		return ticker;
+	}
+
+	public void setTicker(String ticker) {
+		this.ticker = ticker;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getNumberOfCoins() {
+		return numberOfCoins;
+	}
+
+	public void setNumberOfCoins(Long numberOfCoins) {
+		this.numberOfCoins = numberOfCoins;
+	}
+
+	public Long getMarketCap() {
+		return marketCap;
+	}
+
+	public void setMarketCap(Long marketCap) {
+		this.marketCap = marketCap;
+	}
+	
+	public static CurrencyBuilder builder() {
+        return new CurrencyBuilder();
+    }
 }
